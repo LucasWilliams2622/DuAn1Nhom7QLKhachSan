d567cbd360fa826e58f2397c9caeab7265013f9f
Lucas_Williams <101234675+LucasWilliams2622@users.noreply.github.com>
2022-11-14 16:44:46 +0700

    lucas 11/14/2022

--- app/src/main/java/com/example/duan1nhom7qlkhachsan/Activity/LoginActivity.java
+++ app/src/main/java/com/example/duan1nhom7qlkhachsan/Activity/LoginActivity.java
@@ -9,12 +9,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.example.duan1nhom7qlkhachsan.MainActivity;
 import com.example.duan1nhom7qlkhachsan.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -28,6 +37,10 @@ import java.util.List;
 public class LoginActivity extends AppCompatActivity {
     long check = 1;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    //Google
+    GoogleSignInClient gsc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,39 +51,11 @@ public class LoginActivity extends AppCompatActivity {
         Button btn_register = findViewById(R.id.btnGoRegister);
         ImageView ivShowPass = findViewById(R.id.ivShowPass);
 
-
-//        ThuThuDAO thuThuDAO = new ThuThuDAO(this);
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = edt_username.getText().toString();
                 String password = edt_password.getText().toString();
-
-//                if (thuThuDAO.checkLogin(username, password)) {
-//                    //save shareprefferences
-//                    SharedPreferences sharedPreferences = getSharedPreferences("THONGTIN", MODE_PRIVATE);
-//                    //truyen 2 gia tri 1 la ten sharepreferences va MODE
-//                    SharedPreferences.Editor editor = sharedPreferences.edit();
-//                    editor.putString("matt", username);
-//                    editor.commit();
-//
-//                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-//
-//                } else {
-//                    Toast.makeText(LoginActivity.this, "Username or password is wrong !!!", Toast.LENGTH_SHORT).show();
- //   //                if (thuThuDAO.checkLogin(username, password)) {
-//                    //save shareprefferences
-//                    SharedPreferences sharedPreferences = getSharedPreferences("THONGTIN", MODE_PRIVATE);
-//                    //truyen 2 gia tri 1 la ten sharepreferences va MODE
-//                    SharedPreferences.Editor editor = sharedPreferences.edit();
-//                    editor.putString("matt", username);
-//                    editor.commit();
-//
-//                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-//
-//                } else {
-//                    Toast.makeText(LoginActivity.this, "Username or password is wrong !!!", Toast.LENGTH_SHORT).show();
- //               }
                 getData();
 
             }
@@ -99,8 +84,74 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        /*
+         *Start: Đăng nhâp bằng Google
+         * Project:https://console.firebase.google.com/project/myproject262-4a887/overview
+         * Created:11/14/2022
+         * By: Lucas
+         * */
+
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(LoginActivity.this, gso);
+        // kiểm tra login google
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(LoginActivity.this);
+        if (account != null) {
+            Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(homeIntent);
+            finish();
+        }
+        ImageView sib = findViewById(R.id.btnGoogle);
+        sib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent googleIntent = gsc.getSignInIntent();
+                googleLauncher.launch(googleIntent);//hientai khoan dang login trong may
+            }
+        });
+
+
+        /*
+         *End: Đăng nhâp bằng Google*/
+
     }
 
+    // nhận kết quả Google SignIn
+    ActivityResultLauncher<Intent> googleLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Intent data = result.getData();
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    try {
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        String email = account.getEmail();
+                        String displayName = account.getDisplayName();
+                        // lưu database (tự làm)
+                        //check neu email chua co trong db thi them
+                        //neu co thi bao loi
+//                        UserDao userDao = new UserDao(LoginActivity.this);
+//                        userDao.register(email, null, 1);
+
+
+                        // chuyển màn hình qua Home
+                        Intent homeIntent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(homeIntent);
+                        finish();
+
+                        Log.d(">>>>>TAG", "onActivityResult: " + displayName);
+                        Log.d(">>>>>TAG", ">>>: " + email);
+
+                    } catch (Exception e) {
+                        Log.d(">>>>>TAG", "onActivityResult error: " + e.getMessage());
+                    }
+                }
+            }
+    );
     public void getData(){
         db.collection("admin")
                 .get()
